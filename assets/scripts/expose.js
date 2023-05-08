9f8230ceb99bd68df9b84f57655236ff1de69baa
Neil Bajaj <nbajaj@ucsd.edu>
2023-05-07 17:40:53 -0700

    got picture to work

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -11,10 +11,6 @@ function init() {
   const volumeIcon = document.querySelector("#volume-controls img");
   const playButton = document.querySelector("#expose button");
 
-  //hornSelect.addEventListener("change", HornChange);
-  //volume.addEventListener("input", VolumeChange);
-  //playButton.addEventListener("click", PlayButtonClick);
-
 hornSelect.addEventListener('change', function() {
   // Set the image and audio source based on the selected horn
   switch(hornSelect.value) {
@@ -50,19 +46,16 @@ volumeSlider.addEventListener('input', function() {
   audio.volume = volumeSlider.value / 100;
 });
 
-// Event listener for when the "Play Sound" button is clicked
-playButton.addEventListener('click', function() {
-  // Play the audio element at the current volume level
+playButton.addEventListener('click', function(){
   audio.play();
+
   
-  // If the Party Horn is selected, shoot confetti
   if (hornSelect.value == 'party-horn') {
-    confetti();
+    jsConfetti.addConfetti();
   }
 });
 }
 
-// Call the initialize function after the DOM has loaded
 window.addEventListener('load', init);
 
 /** 
